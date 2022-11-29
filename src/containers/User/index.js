e8c53cdd579e566858ddd777a9e85bd54f6961f9
omar-afifi <omar.afifi@vodafone.com>
2022-11-29 19:56:56 +0200

    fix protected routes

--- src/containers/User/index.js
+++ src/containers/User/index.js
@@ -6,12 +6,12 @@ export const UserContext = createContext();
 
 import Layout from '../../components/common/BasicLayout'
 import Loader from '../../components/common/Loader'
-
+const ProtectedRoutes = ["account"]
 export function UserProvider(props) {
 	const router = useRouter()
 	const [user, setUser] = useState({});
-	const [userType , setUSerType] = useState('');
-	const [loaded, setLoaded] = useState(true);
+	const [userType, setUSerType] = useState('');
+	const [loaded, setLoaded] = useState(false);
 	const [loading, setLoading] = useState(false);
 	const [loggedIn, setLoggedIn] = useState(false);
 	const [currentRoute, setCurrentRoute] = useState("");
@@ -22,53 +22,65 @@ export function UserProvider(props) {
 	const [update, setUpdate] = useState(0);
 	const [cartLength, setCartLength] = useState(0);
 	const [profileEditedData, setProfileEfitedData] = useState({});
-	const[categories,setUSerCategories] = useState([])
+	const [categories, setUSerCategories] = useState([])
 	const [adOptions, setAdOptions] = useState([]);
 
-	const onRouteChange = (url, force) => {
-		setCurrentRoute(url);
-		return;
-	};
-
-
-	useEffect(() => {
-		onRouteChange(router.route);
-		router.events.on('routeChangeComplete', onRouteChange);
-		localForage.getItem('cart').then(async data => {
-			if(!!data) {
-				setCart(data || {});
-				setCartLength(Object.keys(data || {}).length);
-			}
-		}).catch(err => {
-
-		});
-		localForage.getItem('user').then(async data => {
-			if(!!data.token && !!data.user) {
+	const getSavedUser = async () => {
+		try {
+			const data = await localForage.getItem('user')
+			if (!!data.token && !!data.user) {
 				setUser(data.user);
 				setUSerType(data.type);
 				setToken(data.token);
 				setLoggedIn(true);
 			}
-		}).catch(err => {
+		} catch (error) {
 			setLoggedIn(false);
-		});
-	}, []);
+		}
+
+	}
+
+	const getSavedCart = async () => {
+		try {
+			const data = await localForage.getItem('cart')
+			if (!!data) {
+				setCart(data || {});
+				setCartLength(Object.keys(data || {}).length);
+			}
+		} catch (error) {
+
+		}
+
+	}
+
+	const init = async () => {
+		await getSavedUser()
+		await getSavedCart()
+		setLoaded(true)
+	}
+
+
+	useEffect(init, []);
 
 	const login = async (values) => {
 		setLoading(true);
 		setUser(values.user);
 		setUSerType(values.type);
 		setToken(values.token);
-		localForage.setItem('user', Object.assign({}, values), () => {
+		localForage.setItem('user', (Object.assign({}, values)), (res) => {
+			console.log("localForage-res", res)
 			setLoggedIn(true);
 			setLoading(false);
-		});
+		}).catch(err => {
+			console.log("localForage-err", err)
+
+		})
 	}
 
 	const updateUser = async (values) => {
 		setLoading(true);
 		setUser(Object.assign(user, values));
-		localForage.setItem('user', {user: Object.assign({}, user, values), token: token, userType: userType}, () => {
+		localForage.setItem('user', { user: Object.assign({}, user, values), token: token, userType: userType }, () => {
 			setLoading(false);
 		});
 	}
@@ -86,22 +98,22 @@ export function UserProvider(props) {
 		cartObject[item.id] = item;
 		setCart(cartObject);
 		setCartLength(Object.keys(cartObject).length);
-		setUpdate(c => c+1);
-		localForage.setItem('cart', cartObject, () => {});
+		setUpdate(c => c + 1);
+		localForage.setItem('cart', cartObject, () => { });
 	};
 
 	const changeQuery = (q) => {
 		setQuery(q);
-		setUpdate(c => c+1);
-		setUpdateQuery(c => c+1)
+		setUpdate(c => c + 1);
+		setUpdateQuery(c => c + 1)
 	};
 
 	const changeCart = (newCart) => {
 		let cartObject = JSON.parse(JSON.stringify(newCart));
 		setCart(cartObject);
 		setCartLength(Object.keys(cartObject).length);
-		setUpdate(c => c+1);
-		localForage.setItem('cart', cartObject, () => {});
+		setUpdate(c => c + 1);
+		localForage.setItem('cart', cartObject, () => { });
 	};
 
 	const removeFromCart = (id) => {
@@ -109,16 +121,16 @@ export function UserProvider(props) {
 		delete cartObject[id];
 		setCart(cartObject);
 		setCartLength(Object.keys(cartObject).length);
-		
-		setUpdate(c => c+1);
-		localForage.setItem('cart', cartObject, () => {});
+
+		setUpdate(c => c + 1);
+		localForage.setItem('cart', cartObject, () => { });
 	};
 
 	const clearCart = () => {
 		setCart({});
 		setCartLength(0);
-		localForage.setItem('cart', {}, () => {});
-		setUpdate(c => c+1);
+		localForage.setItem('cart', {}, () => { });
+		setUpdate(c => c + 1);
 	};
 
 	const logout = async (state) => {
@@ -132,12 +144,21 @@ export function UserProvider(props) {
 	}
 
 
+	const isProtectedRoute = ProtectedRoutes.filter(route => router.route.includes(route))
+
+	if (!loggedIn && loaded && isProtectedRoute.length) {
+		router.replace('/login')
+		return null
+	}
 	return <UserContext.Provider
-		value={{login, logout, addToCart, changeCart, editProfile, updateUser, cartLength, updateQuery, query, changeQuery, update, removeFromCart, clearCart, cart, profileEditedData, userCategories , categories, user,userType, token, loggedIn, loaded,adOptions
-			,setAdOptions }}
+		value={{
+			login, logout, addToCart, changeCart, editProfile, updateUser, cartLength, updateQuery, query, changeQuery, update, removeFromCart, clearCart, cart, profileEditedData, userCategories, categories, user, userType, token, loggedIn, loaded, adOptions
+			, setAdOptions
+		}}
 	>
 		{loaded ? props.children : <Layout>
 			<Loader />
 		</Layout>}
+
 	</UserContext.Provider>
 }
